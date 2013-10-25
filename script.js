586a0763ce1153b685ccf50b59cbb3916dc5e301
Nitharshaan Thevarajah <nitharshaan@gmail.com>
2013-10-25 15:06:18 +0200

    startup

---
+++ script.js
@@ -0,0 +1,7 @@
+$(function() {
+	$( "#tabs" ).tabs();
+    $( ".tabs-bottom .ui-tabs-nav, .tabs-bottom .ui-tabs-nav > *" )
+    .removeClass( "ui-corner-all ui-corner-top" )
+    .addClass( "ui-corner-bottom" );
+}); 
+
