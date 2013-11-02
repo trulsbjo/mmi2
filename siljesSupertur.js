12cbc88705f9099b9a1f8b1ce09c12593e1502dd
Nitharshaan Thevarajah <nitharshaan@gmail.com>
2013-11-02 16:47:22 +0100

    gaa tur knapp fungerer

--- siljesSupertur.js
+++ siljesSupertur.js
@@ -5,5 +5,18 @@ function addToFavorite(){
 	else{
 		$(".addToFavorite").attr("src", "img/star_empty.jpg");
 	}
-	
+}
+
+
+function startTracking(){
+	$(".bar-title").remove();
+	$(".bar-tab").remove();
+	$(".slider").hide();
+	$(".content").css({
+		"background-image":"url('img/map.jpg')",
+		"-webkit-background-size": "cover",
+  		"-moz-background-size": "cover",
+ 		"-o-background-size": "cover",
+  		"background-size": "cover"
+	});
 }
