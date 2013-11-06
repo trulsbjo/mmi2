d7e6e10c27f5e9e9e5f1daaf66179a6a9b7d5d35
Odd Fredrik Mørch Rogstad <oddfredrikrogstad@gmail.com>
2013-11-06 07:09:58 +0100

    div logikk

--- opprett_tracking.js
+++ opprett_tracking.js
@@ -1,4 +1,5 @@
 var teller = 1;
+var filterVal = 'blur(0px)';
 
 function next(){
 	alert(teller);
@@ -7,46 +8,63 @@ function next(){
 	teller++;
 }
 
-function openForm(){
-	var filterVal = 'blur(2px)';
-	$("#bilde"+teller).css({
-		'filter': filterVal,
-		'-webkit-filter': filterVal,
-		'-moz-filter': filterVal,
-		'-o-filter': filterVal,
-		'-ms-filter': filterVal
-	});
+function openMilepal(){
+	blurMap();
+	disableButton("avsluttknapp", true);
 	$("#milepalform").css('z-index', 2);
 	$("#milepalform").css({"display" : "inline"});
 	$("#bilde"+teller).prop('disabled', true);
+	disableButton("avsluttknapp", true);
 }
 
-function closeForm(){
-	var filterVal = 'blur(0px)';
-	$("#bilde"+teller).css({
-		'filter': filterVal,
-		'-webkit-filter': filterVal,
-		'-moz-filter': filterVal,
-		'-o-filter': filterVal,
-		'-ms-filter': filterVal
-	});
+function closeMilepal(){
 	$("#milepalform").css({"display" : "none"});
 	$("#bilde"+teller).prop('disabled', false);
+	unBlurMap();
+	disableButton("avsluttknapp", false);
 }
 
-function okForm(){
-	var filterVal = 'blur(0px)';
-	$("#bilde"+teller).css({
-		'filter': filterVal,
-		'-webkit-filter': filterVal,
-		'-moz-filter': filterVal,
-		'-o-filter': filterVal,
-		'-ms-filter': filterVal
-	});
+function setMilepal(){
 	$("#milepalform").css({"display" : "none"});
 	$("#bilde"+teller).prop('disabled', false);
+	unBlurMap();
+	disableButton("avsluttknapp", false);
 	next();
 }
 
 function exit(){
+	var exitBoolean = confirm("Sikker på at du vil avslutte?");
+	if(exitBoolean == true){
+		blurMap();
+		$("#avsluttform").css('z-index', 2);
+		$("#avsluttform").css({"display" : "inline"});
+		$("#bilde"+teller).prop('disabled', true);
+		disableButton("milepalknapp", true);
+	} 
+}
+
+function disableButton(nameOfButton, booleanButton){
+	$("#"+ nameOfButton).prop('disabled', booleanButton);
+}
+
+function unBlurMap(){
+	filterVal = 'blur(0px)';
+	$("#bilde"+teller).css({
+		'filter': filterVal,
+		'-webkit-filter': filterVal,
+		'-moz-filter': filterVal,
+		'-o-filter': filterVal,
+		'-ms-filter': filterVal
+	});
+}
+
+function blurMap(){
+	filterVal = 'blur(2px)';
+	$("#bilde"+teller).css({
+		'filter': filterVal,
+		'-webkit-filter': filterVal,
+		'-moz-filter': filterVal,
+		'-o-filter': filterVal,
+		'-ms-filter': filterVal
+	});
 }
